ccf18a6f723bf34680b5032c030515ef4b9a2327
Anneleen Swanepoel <anneleen.swanepoel@gmail.com>
2026-04-05 09:04:08 -0500

    Added code but getting an error

--- demo.py
+++ demo.py
@@ -59,26 +59,21 @@ if __name__ == '__main__':
     # Parse the command-line arguments into a 'dict'-like container
     args = parser.parse_args()
 
-    # Parse the command-line arguments into a 'dict'-like container
-    args = parser.parse_args()
-
     if args.quiet:
         print("Shhh. Be vewy, vewy quiet, I'm hunting wabbits...")
     else:
         # What does `args` object "look" like?
-        print(
-            "The args after being processed by the argparse parser object:\n",
-            args)
-
-        print("Paths:", args.file_path)
-        print("Number:", args.number)
-        print("Threshold:", args.threshold)
-        print("I am cool?", args.i_am_cool)
-
-    # What does `args` object "look" like?
     print(
         "The args after being processed by the argparse parser object:\n",
         args)
+
+    print("Paths:", args.file_path)
+    print("Number:", args.number)
+    print("Threshold:", args.threshold)
+    print("I am cool?", args.i_am_cool)
+
+    # What does `args` object "look" like?
+    
 
     # We access the arguments specified on the command line (or the default
     # values if they were not used) by using `args.` syntax.
@@ -86,12 +81,10 @@ if __name__ == '__main__':
     # For positional arguments (like the first `add_argument` above), we access
     # the value of the arugment using the name we provided as the first
     # argument ("paths")
-    print("Paths:", args.file_path)
+    
 
     # For keyword arguments (like all the rest of the `add_argument` calls
     # above), we access the value of the argument using the the long form of
     # the keyword flag (removing the first two dashes and replacing any other
     # dashes with underscores)
-    print("Number:", args.number)
-    print("Threshold:", args.threshold)
-    print("I am cool?", args.i_am_cool)
+    
